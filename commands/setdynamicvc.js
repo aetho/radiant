654833293192f790cc9c14bfdad197f40365b453
Phuoc Nguyen <10571335+aetho@users.noreply.github.com>
2022-03-05 03:02:31 +1000

    Implemented front-end of setdynamicvc command

---
+++ commands/setdynamicvc.js
@@ -0,0 +1,81 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageSelectMenu } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("setdynamicvc")
+		.setDescription(
+			"Sets the provided voice channel as a dynamic voice channel."
+		),
+	async execute(commandInteraction) {
+		// Construct select menu
+		const channels = await commandInteraction.guild.channels.fetch();
+		const voiceChannels = channels.filter((el) => el.type === "GUILD_VOICE");
+		const row = new MessageActionRow().addComponents(
+			new MessageSelectMenu()
+				.setCustomId("setdynamicvcselect")
+				.setPlaceholder("Select Channel")
+				.addOptions(
+					voiceChannels.map((el) => {
+						return {
+							label: `${el.name}`,
+							description: `ID: ${el.id}`,
+							value: el.id,
+						};
+					})
+				)
+		);
+
+		// Send select menu
+		const msg = await commandInteraction.reply({
+			content: "Select dynamic voice channel",
+			components: [row],
+			fetchReply: true,
+		});
+
+		// Create select collector
+		const collector = msg.createMessageComponentCollector({
+			componentType: "SELECT_MENU",
+			time: 7000,
+		});
+
+		// Handle selections
+		const replies = [];
+		collector.on("collect", async (selectInteraction) => {
+			if (selectInteraction.user.id === commandInteraction.user.id) {
+				const choice = voiceChannels.get(selectInteraction.values[0]).name;
+
+				// TODO: Store/update dynamic voice channel
+
+				const reply = await selectInteraction.reply({
+					content: `Dynamic voice channel has been set to \`${choice}\`.`,
+					fetchReply: true,
+				});
+
+				replies.push(reply);
+			} else {
+				selectInteraction.reply({
+					content: "These buttons aren't for you!",
+					ephemeral: true,
+				});
+			}
+		});
+
+		collector.on("end", async () => {
+			// Update inital reply to reflect selection
+			const editContent =
+				replies.length < 1
+					? "No channel was selected."
+					: replies[replies.length - 1].content;
+			await msg.edit({
+				content: editContent,
+				components: [],
+			});
+
+			// Remove clutter
+			replies.forEach(async (el) => {
+				await el.delete();
+			});
+		});
+	},
+};
--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+const { Client, Intents, Collection } = require("discord.js");
 const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs
+	.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async (interaction) => {
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		if (interaction.isCommand()) {
+			await command.execute(interaction);
+		} else if (interaction.isSelectMenu()) {
+			await command.select(interaction);
+		} else if (interaction.isButton()) {
+			await command.button(interaction);
+		}
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true,
+		});
+	}
+});
+
 client.once("ready", () => {
-	console.log("Ready!");
+	console.log("Ready");
 });
 
 client.login(token);
